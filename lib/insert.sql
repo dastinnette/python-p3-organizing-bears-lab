7d5e59ca2bbbfb1f4d3c82a65efed8f30776a94e
David Stinnette <dastinnette@gmail.com>
2023-11-02 14:09:45 -0400

    Completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,31 @@
+-- Insert an unnamed bear into the bears table
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, NULL, NULL, NULL, NULL, 0);
+
+-- Insert bear 1
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 1, 'M', 'Brown', 'Friendly', 1);
+
+-- Insert bear 2
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 2, 'M', 'Black', 'Energetic', 1);
+
+-- Insert bear 3
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 3, 'F', 'White', 'Playful', 1);
+
+-- Insert bear 4
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 4, 'M', 'Brown', 'Disciplined', 1);
+
+-- Insert bear 5
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 5, 'F', 'Black', 'Sweet', 1);
+
+-- Insert bear 6
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 6, 'M', 'Green', 'Grumpy', 0);
+
+-- Insert bear 7
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 7, 'F', 'White', 'Sweet', 1);
